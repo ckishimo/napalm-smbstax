04054746d272493104f388f44047dc277b19ba68
Carles Kishimoto <carles.kishimoto@gmail.com>
2019-04-26 11:42:50 +0200

    update setup files

--- setup.py
+++ setup.py
@@ -5,27 +5,27 @@ import uuid
 from setuptools import setup, find_packages
 from pip.req import parse_requirements
 
-__author__ = 'David Barroso <dbarrosop@dravetech.com>'
+__author__ = 'Carles Kishimoto <carles.kishimoto@gmail.com>'
 
 install_reqs = parse_requirements('requirements.txt', session=uuid.uuid1())
 reqs = [str(ir.req) for ir in install_reqs]
 
 setup(
-    name="napalm-skeleton",
+    name="napalm-smbstax",
     version="0.1.0",
     packages=find_packages(),
-    author="David Barroso",
-    author_email="dbarrosop@dravetech.com",
+    author="Carles Kishimoto",
+    author_email="carles.kishimoto@gmail.com",
     description="Network Automation and Programmability Abstraction Layer with Multivendor support",
     classifiers=[
         'Topic :: Utilities',
          'Programming Language :: Python',
-         'Programming Language :: Python :: 2',
-         'Programming Language :: Python :: 2.7',
+         'Programming Language :: Python :: 3',
+         'Programming Language :: Python :: 3.6',
         'Operating System :: POSIX :: Linux',
         'Operating System :: MacOS',
     ],
-    url="https://github.com/napalm-automation/napalm-skeleton",
+    url="https://github.com/napalm-automation/napalm-smbstax",
     include_package_data=True,
     install_requires=reqs,
 )
