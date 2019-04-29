db9b17268eb33d74a56196d20c687432836874e8
Carles Kishimoto <carles.kishimoto@gmail.com>
2019-04-29 15:15:07 +0200

    apply black formatting

--- napalm_smbstax/smbstax.py
+++ napalm_smbstax/smbstax.py
@@ -230,27 +230,25 @@ class SMBStaXDriver(NetworkDriver):
         output = {}
 
         _data = napalm.base.helpers.textfsm_extractor(
-            self,
-            "statistics",
-            self.device.send_command("show interface * statistics"),
+            self, "statistics", self.device.send_command("show interface * statistics")
         )
         if _data:
             for iface in _data:
-                name =iface['interface']
+                name = iface["interface"]
 
                 output[name] = {}
-                output[name]['tx_multicast_packets'] = iface['tx_multicast']
-                output[name]['rx_multicast_packets'] = iface['rx_multicast']
-                output[name]['tx_broadcast_packets'] = iface['tx_broadcast']
-                output[name]['rx_broadcast_packets'] = iface['rx_broadcast']
-                output[name]['tx_unicast_packets'] = iface['tx_octets']
-                output[name]['rx_unicast_packets'] = iface['rx_octets']
+                output[name]["tx_multicast_packets"] = iface["tx_multicast"]
+                output[name]["rx_multicast_packets"] = iface["rx_multicast"]
+                output[name]["tx_broadcast_packets"] = iface["tx_broadcast"]
+                output[name]["rx_broadcast_packets"] = iface["rx_broadcast"]
+                output[name]["tx_unicast_packets"] = iface["tx_octets"]
+                output[name]["rx_unicast_packets"] = iface["rx_octets"]
                 # FIXME: drops ?
-                output[name]['tx_discards'] = 0
-                output[name]['rx_discards'] = 0
-                output[name]['tx_errors'] = -1
-                output[name]['rx_errors'] = iface['crc']
-                output[name]['tx_octets'] = iface['tx_octets']
-                output[name]['rx_octets'] = iface['rx_octets']
+                output[name]["tx_discards"] = 0
+                output[name]["rx_discards"] = 0
+                output[name]["tx_errors"] = -1
+                output[name]["rx_errors"] = iface["crc"]
+                output[name]["tx_octets"] = iface["tx_octets"]
+                output[name]["rx_octets"] = iface["rx_octets"]
 
         return output
